ca5dc02e587cea3ad96404d89510f2419ef50e58
unknown <ms568203@gmail.com>
2020-04-19 15:48:43 +0530

    Refactor. Added Usecase-6 show the number time dice Roll to win the game and position after every dice roll

--- snakeAndLadderSimulation.sh
+++ snakeAndLadderSimulation.sh
@@ -5,15 +5,17 @@ noPlay=0;
 ladder=1;
 snake=2;
 winningPosition=100;
-
+diceCounter=1;
 function ladder(){
 	position=$((1+RANDOM%6));
 	#tempResult=$(($player1+$positon));
 	if (( $(($player1+$position)) > $winningPosition ))
 	then
 		player1=$(($player1+0));
+		positionArray[((diceCounter++))]=$player1;
 	else
 		player1=$(($player1+$position));
+		positionArray[((diceCounter++))]=$player1;
 	fi
 }
 
@@ -22,8 +24,10 @@ function snake(){
         if (( $player1 > $position ))
         then
        		player1=$(($player1-$position))
+		positionArray[((diceCounter++))]=$player1;
         else
         	player1=$(($player1 + 0));
+		positionArray[((diceCounter++))]=$player1;
         fi
 }
 
@@ -49,3 +53,5 @@ do
 done
 
 echo $player1;
+echo "Total number of dies to win the game: " ${#positionArray[@]};
+echo "Position of the player after each die Roll: " ${positionArray[@]};
