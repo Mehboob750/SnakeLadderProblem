7cf024e68f2fffda6286bafb94c09138edf3b8bd
unknown <ms568203@gmail.com>
2020-04-19 12:11:00 +0530

    Refactor. Added Usecase3-player checks for No Play,Ladder or Snake

--- snakeAndLadderSimulation.sh
+++ snakeAndLadderSimulation.sh
@@ -1,4 +1,31 @@
 #!/bin/bash -x
 position=0;
 player1=$position;
-palyer1=$((1+RANDOM%6));
+
+noPlay=0;
+ladder=1;
+snake=2;
+option=$((RANDOM%3));
+case $option in
+	$noPlay )
+		player1=$(($player1 + 0));
+		;;
+
+	$ladder )
+		position=$((1+RANDOM%6));
+		player1=$(($player1+$position))
+		;;
+
+	$snake )
+		if (( $player1 > 0 ))
+		then
+			position=$((1+RANDOM%6));
+			player1=$(($player1-$position))
+		else
+			player1=$(($player1 + 0));
+		fi
+		;;
+	* )
+		echo "Invalid Selection"
+		;;
+esac
