7ab4b126aae198d3197b0dcb691009056ba4b401
unknown <ms568203@gmail.com>
2020-04-19 12:57:58 +0530

    Refactor. Added usecase7- player plays upto he reaches the winning position

--- snakeAndLadderSimulation.sh
+++ snakeAndLadderSimulation.sh
@@ -1,31 +1,51 @@
 #!/bin/bash -x
 position=0;
 player1=$position;
-
 noPlay=0;
 ladder=1;
 snake=2;
-option=$((RANDOM%3));
-case $option in
-	$noPlay )
-		player1=$(($player1 + 0));
-		;;
+winningPosition=100;
 
-	$ladder )
-		position=$((1+RANDOM%6));
-		player1=$(($player1+$position))
-		;;
+function ladder(){
+	position=$((1+RANDOM%6));
+	#tempResult=$(($player1+$positon));
+	if (( $(($player1+$position)) > $winningPosition ))
+	then
+		player1=$(($player1+0));
+	else
+		player1=$(($player1+$position));
+	fi
+}
 
-	$snake )
-		if (( $player1 > 0 ))
-		then
-			position=$((1+RANDOM%6));
-			player1=$(($player1-$position))
-		else
+function snake(){
+	position=$((1+RANDOM%6));
+        if (( $player1 > $position ))
+        then
+       		player1=$(($player1-$position))
+        else
+        	player1=$(($player1 + 0));
+        fi
+}
+
+while (( $player1 != $winningPosition))
+do
+	option=$((RANDOM%3));
+	case $option in
+		$noPlay )
 			player1=$(($player1 + 0));
-		fi
-		;;
-	* )
-		echo "Invalid Selection"
-		;;
-esac
+			;;
+
+		$ladder )
+			ladder
+			;;
+
+		$snake )
+			snake
+			;;
+		* )
+			echo "Invalid Selection"
+			;;
+	esac
+done
+
+echo $player1;
