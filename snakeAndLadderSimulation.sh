ecfc04140b452eb00133073b82e20f4dabeaae3c
unknown <ms568203@gmail.com>
2020-04-20 09:08:19 +0530

    Refactor. Added usecase7-play the game for two players and show the winner

--- snakeAndLadderSimulation.sh
+++ snakeAndLadderSimulation.sh
@@ -1,57 +1,89 @@
 #!/bin/bash -x
 position=0;
-player1=$position;
+#player=$position;
 noPlay=0;
 ladder=1;
 snake=2;
 winningPosition=100;
 diceCounter=1;
+diceRollPCount=1;
 function ladder(){
 	position=$((1+RANDOM%6));
 	#tempResult=$(($player1+$positon));
-	if (( $(($player1+$position)) > $winningPosition ))
+	if (( $(($player+$position)) > $winningPosition ))
 	then
-		player1=$(($player1+0));
-		positionArray[((diceCounter++))]=$player1;
+		player=$(($player+0));
+		positionArray[((diceCounter++))]=$player;
 	else
-		player1=$(($player1+$position));
-		positionArray[((diceCounter++))]=$player1;
+		player=$(($player+$position));
+		positionArray[((diceCounter++))]=$player;
 	fi
+	echo $player;
 }
 
 function snake(){
 	position=$((1+RANDOM%6));
-        if (( $player1 > $position ))
-        then
-       		player1=$(($player1-$position))
-		positionArray[((diceCounter++))]=$player1;
-        else
-        	player1=$(($player1 + 0));
-		positionArray[((diceCounter++))]=$player1;
-        fi
+   if [[ $player -gt $position ]]
+   then
+   	player=$(($player-$position))
+		positionArray[((diceCounter++))]=$player;
+	else
+   	player=$(($player + 0));
+		positionArray[((diceCounter++))]=$player;
+	fi
+  echo $player;
 }
 
-while (( $player1 != $winningPosition))
+function checkNoplayLadderSnake(){
+		option=$((RANDOM%3));
+		case $option in
+			$noPlay )
+				player=$(($player + 0));
+			   echo $player;
+				;;
+
+			$ladder )
+				ladder
+				;;
+
+			$snake )
+				snake
+				;;
+			* )
+				echo "Invalid Selection"
+				;;
+		esac
+}
+
+function displayWinPOsition(){
+	echo $player;
+	echo "Total number of dice roll to win the game: " ${#positionArray[@]};
+	echo "Position of the player after each die Roll: " ${positionArray[@]};
+}
+
+
+#playe for multiple players
+#player1=0;
+#player2=0;
+
+while [[ $player1 -ne $winningPosition || $player2 -ne $winningPosition ]]
 do
-	option=$((RANDOM%3));
-	case $option in
-		$noPlay )
-			player1=$(($player1 + 0));
-			;;
+	if (( $(($diceRollPCount%2)) == 0 ))
+	then
+		player1="$( checkNoplayLadderSnake $player1 )";
+		(( diceRollPCount++ ));
 
-		$ladder )
-			ladder
-			;;
-
-		$snake )
-			snake
-			;;
-		* )
-			echo "Invalid Selection"
-			;;
-	esac
+		sleep 5s;
+	else
+		player2="$( checkNoplayLadderSnake $player2 )"
+		(( diceRollPCount++ ));
+		sleep 5s;
+	fi
 done
 
-echo $player1;
-echo "Total number of dies to win the game: " ${#positionArray[@]};
-echo "Position of the player after each die Roll: " ${positionArray[@]};
+if (( $player1 == $winningPosition ))
+then
+	echo "Player1 win!!!";
+else
+	echo "Player2 win!!!";
+fi
